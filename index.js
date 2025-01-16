9a5600bae53a801d72279a37bc9376c5b1f1362e
Mickey <mickeyrossgame@gmail.com>
2025-01-16 13:07:35 +0200

    finish

---
+++ index.js
@@ -0,0 +1,16 @@
+function calculateSeconds() {
+
+	const hours = document.getElementById("hours").value;
+
+
+	if (!hours || hours < 0) {
+		document.getElementById("result").textContent =
+			"Введите коректное количество часов.";
+		return;
+	}
+
+	const seconds = hours * 60 * 60;
+
+	document.getElementById("result").textContent =
+		`в ${hours} часов ${seconds} секунд.`;
+}
