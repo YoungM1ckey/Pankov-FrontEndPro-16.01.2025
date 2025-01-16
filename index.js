3a3242ec3c35982a0a33fd3c710e6e670045ef6e
Mickey <mickeyrossgame@gmail.com>
2025-01-16 21:35:34 +0200

    addalert

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ function calculateSeconds() {
 	if (!hours || hours < 0) {
 		document.getElementById("result").textContent =
 			"Введите коректное количество часов.";
+      alert("Введите корректное количество часов.");
 		return;
 	}
 
 	const seconds = hours * 60 * 60;
+
+  	alert(`В ${hours} часах ${seconds} секунд.`);
 
 	document.getElementById("result").textContent =
 		`В ${hours} часах ${seconds} секунд.`;
